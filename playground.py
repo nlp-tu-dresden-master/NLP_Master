5c36bde84587d1881b94524b0d3177071bf028ee
Richard Horn <richard.horn@mailbox.tu-dresden.de>
2018-05-01 10:38:21 +0200

    Added function to corpora to create all corpora for new algorithm. Added frequency distribution for word sense words

--- playground.py
+++ playground.py
@@ -1,15 +1,15 @@
 from models import Corpora
 from models import Topic_Engine
 
-corpus = Corpora(paths=["01_data/01_Clustering_definitions"], names=["Clustering"])
+corp = Corpora(["Clustering"], ["01_data/01_Clustering_definitions"])
+corp.build_all_corpora_for_new_algorithm_type("Classification", "01_data/02_Classification_definitions")
 
 # How i would access the attributes
-print(corpus.token_corpora)
-print(corpus.raw_corpora)
-print(corpus.document_corpora)
+# print(corp.token_corpora)
+# print(corp.raw_corpora)
+# print(corp.document_corpora)
 
-# Prove that class attributes are empty
-print(Corpora.document_corpora)
-print(Corpora.token_corpora)
-print(Corpora.raw_corpora)
-
+engine = Topic_Engine(corpora=corp)
+# engine.get_most_common_tokens("clustering", 15)
+# engine.get_most_common_tokens("Classification", 15)
+engine.get_frequency_distribution_of_word_meanings("clustering", 15)
