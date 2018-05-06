1fa2c9c1bc7808e322ab6fd7a7cc5c5afe0cc0c7
MonteChristo46 <Dhoeschele@gmx.net>
2018-05-06 15:03:32 +0200

    Init changed according to new class

--- nlp_master/__init__.py
+++ nlp_master/__init__.py
@@ -1,4 +1,4 @@
 from nlp_master.Corpora import Corpora
 from nlp_master.Topic_Engine import Topic_Engine
 from nlp_master.Operation import Operation
-from nlp_master.TopicSet import TopicSet
+from nlp_master.TopicSet import *
